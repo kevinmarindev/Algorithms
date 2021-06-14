5d43d6cc2e7588874f561e1b302acb134bb98061
kevinmarindev <kevin.marin7@hotmail.com>
2021-06-14 17:32:07 -0500

    second algorithm.

---
+++ reverseString/index-START2.js
@@ -0,0 +1,37 @@
+/* CHALLENGE
+Given a string of text, write an algorithm that returns the text received in a reversed format. 
+E.g reverseString('algorithms') // should return 'smhtirogla'
+*/
+
+
+// binding methods// 
+// function reverseString(text) {
+//     return text.split('').reverse().join('')
+// }
+
+//es6 binding methods//
+// function reverseString(text) {
+//     return [...text].reverse().join('')
+// }
+
+//for loop 
+// function reverseString(text) {
+//     let result = ""
+//     for (let i = text.length - 1; i >= 0; i--) {
+//         result += text[i]
+//     }
+//     return result
+// }
+
+//callback / recursive way
+ function reverseString(text) {
+    return text === '' ? '' : reverseString(text.substr(1)) + text[0]
+}
+
+
+//minor changes added 
+
+changes///fffdfdfdff
+
+
+module.exports = reverseString
