b0f54581197cd6ed78777c23f79ad689fa4019e4
kevinmarindev <kevin.marin7@hotmail.com>
2021-08-04 23:57:19 -0500

    day 2 of solving this algo until i get them all right.

--- reverseString/index-START2.js
+++ reverseString/index-START2.js
@@ -2,38 +2,47 @@
 Given a string of text, write an algorithm that returns the text received in a reversed format. 
 E.g reverseString('algorithms') // should return 'smhtirogla'
 */
-
+//day 2
 
 // binding methods// 
-// function reverseString(text) {
-//     return text.split('').reverse().join('')
-// }
+function reverseString(text) {
+   return [...text].reverse().join('');
+}
+
 
 //es6 binding methods//
+//use spread operator to create an array 
 // function reverseString(text) {
-//     return [...text].reverse().join('')
+//     return [...text].reverse('').join('');
 // }
 
 //for loop 
 // function reverseString(text) {
 //     let result = ""
 //     for (let i = text.length - 1; i >= 0; i--) {
+//         console.log(text[i])
 //         result += text[i]
+        
 //     }
 //     return result
 // }
 
+console.log(reverseString('uoiea'))
+
 //callback / recursive way
- function reverseString(text) {
-    return text === '' ? '' : reverseString(text.substr(1)) + text[0]
-}
+//  function reverseString(text) {
+//     return text === '' ? '' : reverseString(text.substr(1)) + text[0]
+// }
 
 
-//minor changes added 
-
-changes///fffdfdfdff
-
-//testting 
+//using reduce
+// function reverseString(text){
+//     return text.split('').reduce((acc, itm) => {
+//         console.log(acc)
+//         return itm + acc 
+//     }
+//         ,'')
+// }
 
 
 module.exports = reverseString
