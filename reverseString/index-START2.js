200214fb11a691024c7ac62d41579002af78302f
kevinmarindev <kevin.marin7@hotmail.com>
2021-08-06 21:15:41 -0500

    third day working with the differernt ways to solve this.

--- reverseString/index-START2.js
+++ reverseString/index-START2.js
@@ -2,47 +2,44 @@
 Given a string of text, write an algorithm that returns the text received in a reversed format. 
 E.g reverseString('algorithms') // should return 'smhtirogla'
 */
-//day 2
+//day 3
 
 // binding methods// 
-function reverseString(text) {
-   return [...text].reverse().join('');
-}
+// function reverseString(text) {
+//    return text.split('').reverse().join('');
+// }
 
 
 //es6 binding methods//
 //use spread operator to create an array 
 // function reverseString(text) {
-//     return [...text].reverse('').join('');
+//     return [...text].reverse().join('')
 // }
-
+   
 //for loop 
 // function reverseString(text) {
-//     let result = ""
-//     for (let i = text.length - 1; i >= 0; i--) {
-//         console.log(text[i])
-//         result += text[i]
-        
-//     }
-//     return result
+//  let result = '';
+//  for (let i = text.length - 1; i >= 0; i--) {
+//      result += text[i]
+//  }
+//  return result 
 // }
 
-console.log(reverseString('uoiea'))
+
 
 //callback / recursive way
 //  function reverseString(text) {
-//     return text === '' ? '' : reverseString(text.substr(1)) + text[0]
+//     return text === '' ? '' : reverseString(text.slice(1)) + text[0]
 // }
+                                                                
 
-
-//using reduce
-// function reverseString(text){
-//     return text.split('').reduce((acc, itm) => {
-//         console.log(acc)
-//         return itm + acc 
-//     }
-//         ,'')
-// }
-
+//using reduce       
+function reverseString(text){
+    return [...text].reduce((acc, item) => {
+        return item + acc
+    }, '')
+}           
+//                                                                                                                  
+            
 
 module.exports = reverseString
