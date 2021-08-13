9b179ce9cec1dd240fff30f79b97dc7626fb23de
kevinmarindev <kevin.marin7@hotmail.com>
2021-08-13 00:40:18 -0500

    day 3 getting easier to solve.

--- vowelsCounter/index-START.js
+++ vowelsCounter/index-START.js
@@ -5,19 +5,19 @@ e.g vowelsCounter('anehizxcv') // will return 3
 //day 3
 
 const vowels = ['a', 'e', 'i', 'o', 'u']
-function vowelsCounter(text) {
-    let counter = 0;
-    for (const letter of text.toLowerCase()) {
-        if(vowels.includes(letter)) counter++
-    }
-    return counter;
-}
-
-// function vowelsCounter(text){
-//     let matches = text.match(/[aeiou]/gi);
-//     if(matches) return matches.length;
-//     else return 0
+// function vowelsCounter(text) {
+//     let counter = 0;
+//     for (const letter of text.toLowerCase()) {
+//         if(vowels.includes(letter)) counter++
+//     }
+//     return counter;
 // }
+
+function vowelsCounter(text){
+    let matches = text.match(/[aeiou]/gi);
+    if(matches) return matches.length;
+    else return 0;
+}
 
 
 module.exports = vowelsCounter;
