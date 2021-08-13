c755de4fd96a56958cfb4783b928e4656109c573
kevinmarindev <kevin.marin7@hotmail.com>
2021-08-12 23:59:43 -0500

    seconf day.

--- vowelsCounter/index-START.js
+++ vowelsCounter/index-START.js
@@ -2,22 +2,22 @@
 Given a string of text, return the number of vowels found within the text
 e.g vowelsCounter('anehizxcv') // will return 3
 */
-//day 1
+//day 2
 
-// const vowels = ['a', 'e', 'i', 'o', 'u']
-// function vowelsCounter(text) {
-//     let counter = 0;
-//     for (const letter of text.toLowerCase()) {
-//         if (vowels.includes(letter)) counter++
-//     }
-//     return counter;
-// }
-
-function vowelsCounter(text){
-    let matches = text.match(/[aeiou]/gi);
-    if(matches) return matches.length
-    else return 0 
+const vowels = ['a', 'e', 'i', 'o', 'u']
+function vowelsCounter(text) {
+    let counter =  0;
+    for (let letter of text.toLowerCase()) {
+        if(vowels.includes(letter)) counter++
+    }
+    return counter;
 }
+
+// function vowelsCounter(text){
+//     let matches = text.match(/[aeiou]/gi);
+//     if(matches) return matches.length
+//     else return 0 
+// }
 
 
 module.exports = vowelsCounter;
