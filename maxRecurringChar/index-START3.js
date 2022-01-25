cbb0f05bfb84a7e2cb7e8f866dde3c1284d56f29
kevinmarindev <kevin.marin7@hotmail.com>
2022-01-25 13:20:09 -0600

    reviewd algo and optimized it

--- maxRecurringChar/index-START3.js
+++ maxRecurringChar/index-START3.js
@@ -5,28 +5,40 @@ e.g maxRecurringChar('aabacada') // will return 'a'
 
 
 
-function maxRecurringChar(text) {
-    let charmap = {};
-    let maxCharVal = 0
-    let maxChar = ''
+// function maxRecurringChar(text) {
+//     let charmap = {};
+//     let maxCharVal = 0
+//     let maxChar = ''
 
-    for (let char of text) {
-        if(charmap.hasOwnProperty(char)){
-            charmap[char]++
-        }else{
-            charmap[char] = 1
-        }
-    }
+//     for (let char of text) {
+//         if(charmap.hasOwnProperty(char)){
+//             charmap[char]++
+//         }else{
+//             charmap[char] = 1
+//         }
+//     }
 
-    for (let char in charmap) {
-        if(charmap[char] > maxCharVal){
-          maxCharVal = charmap[char]
-          maxChar = char
-        }
+//     for (let char in charmap) {
+//         if(charmap[char] > maxCharVal){
+//           maxCharVal = charmap[char]
+//           maxChar = char
+//         }
+//     }
+//  return maxChar
+// }
+
+function maxRecurringChar(str){
+    let objToHoldChars = {}
+    let charVal = 0
+    let theChar = ''
+    for (const char of str) {
+        if(objToHoldChars[char]) objToHoldChars[char]++
+        else objToHoldChars[char] = 1
     }
- return maxChar
+    for (const char in objToHoldChars) {
+        if(objToHoldChars[char] > charVal) charVal = objToHoldChars[char], theChar = char;
+    }
+    return theChar
 }
-
-
 
 module.exports = maxRecurringChar;
