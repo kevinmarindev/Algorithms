dbacf3e596de7a945a61a1fd3d2715058b5fe80c
kevinmarindev <kevin.marin7@hotmail.com>
2022-02-15 00:10:55 -0600

    revisiting till i kill this algo

--- vowelsCounter/index-START.js
+++ vowelsCounter/index-START.js
@@ -6,18 +6,31 @@ e.g vowelsCounter('anehizxcv') // will return 3
 
 const vowels = ['a', 'e', 'i', 'o', 'u']
 // for of method
-function vowelsCounters(text) {
-   let count = 0
-   for (const char of text) {
-       vowels.includes(char.toLowerCase()) ? count++ : ''
-   }
-   return counts
+// function vowelsCounters(text) {
+//    let count = 0
+//    for (const char of text) {
+//        vowels.includes(char.toLowerCase()) ? count++ : ''
+//    }
+//    return counts
+// }
+function vowelsCounters(text){
+    let amount = 0;
+    for (const char of text) {
+        vowels.includes(char.toLowerCase()) ? amount++ : ''
+    }
+    return amount
 }
 
+// function vowelsCounters(text){
+//     let regEx = /[aeiou]/ig
+//     let array = text.match(regEx)
+//     return array ? array.length : 0
+// }
+
 function vowelsCounters(text){
-    let regEx = /[aeiou]/ig
-    let array = text.match(regEx)
-    return array ? array.length : 0
+    let reggex = /[aeiou]/ig
+    let array = text.match(reggex)
+    return array ? array.length : 0 
 }
 //looked over this algo again 
 
