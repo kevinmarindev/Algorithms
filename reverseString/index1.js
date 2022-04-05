4fcc97eacb83b2136e23802e765c594dec5f8e69
kevinmarindev <kevin.marin7@hotmail.com>
2022-04-05 02:34:29 -0500

    using a for loop

--- reverseString/index1.js
+++ reverseString/index1.js
@@ -4,8 +4,17 @@
 //     return str.split('').reverse().join('')
 // }
 
-function rvrsStr(str){
-    return [...str].reverse().join('')
-}
+// function rvrsStr(str){
+//     return [...str].reverse().join('')
+// }
 
+
+function rvrsStr(str){
+    let rstr = ''
+    for (let i = str.length - 1; i >= 0; i--) {
+        // console.log(str[i])
+       rstr += str[i]
+    }
+    return rstr
+}
 console.log(rvrsStr('kevin'))
