40c9231d3935db20c0780fac39b60b9af3f5483b
kevinmarindev <kevin.marin7@hotmail.com>
2022-04-10 18:00:39 -0500

    solved using for of loop

--- vowelsCounter/index1.js
+++ vowelsCounter/index1.js
@@ -1,4 +1,15 @@
+// function vowelCounter(str){
+//     return str.match(/[aeiou]/gi).length
+// }
+
+let vowels = ['a', 'e', 'i', 'o', 'u']
+
 function vowelCounter(str){
-    return str.match(/[aeiou]/gi).length
+    let counter = 0
+    for (const char of str) {
+        if(vowels.includes(char.toLowerCase())) counter++
+    }
+    return counter
 }
+
 console.log(vowelCounter('aeiourrr'))
