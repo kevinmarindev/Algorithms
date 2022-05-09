e31452a7be59ea3a4d4a09a26a2326d63bd5ddec
kevinmarindev <kevin.marin7@hotmail.com>
2022-05-08 23:50:53 -0500

    solved algo with splice method remember that arrays are copied by reference unless you use spread operator

--- chunkArray/index-START.js
+++ chunkArray/index-START.js
@@ -1,8 +1,17 @@
 
 
 function chunkArray(array, size) {
-    // Code goes here
+    let arrOne = [...array]
+    let finalArr = []
+    let i = 0
+    while( i < (array.length / size)){
+        finalArr.push(arrOne.splice(0, size))
+        i++
+    }
+    return finalArr
 }
 
+console.log(chunkArray([1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13], 5))
+console.log(chunkArray([1, 2, 3, 4, 5, 6, 7, 8, 9, 10], 2))
 
 module.exports = chunkArray
