e5b08567c6649dd71642a15e094015af886747a2
kevinmarindev <kevin.marin7@hotmail.com>
2022-05-11 01:34:46 -0500

    solved with for loop

--- factorial/index-START.js
+++ factorial/index-START.js
@@ -4,7 +4,16 @@ the provided integer(n). E.g
     factorial(5) // should return 120
 */
 
+function factorial(n){
+    if(n == 1) return 1
+    let result = n
+    for (let i = n - 1; i > 0; i--) {
+        console.log('yes')
+        result *= i 
+    }
 
+    return result 
+}
 
-
+console.log(factorial(5))
 module.exports = factorial
